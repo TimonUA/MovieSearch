a573be474d777488e705c1ed7c5b7b46e765a8e2
TimonUA <57059072+TimonUA@users.noreply.github.com>
2020-02-12 15:07:22 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,9 +8,9 @@ $(document).ready(() => {
 let baseURL='https://api.themoviedb.org/3/';
 let configData = null;
 let baseImageURL = null;
-let APIKEY=f7fae4e159c02dce183099b33d708df6;
+let APIKEY='f7fae4e159c02dce183099b33d708df6';
 let getConfig = function(){
-    let url = ''.concat(baseURL, 'configuration?api_key=',APIKEY);
+    let url = "".concat(baseURL, 'configuration?api_key=',APIKEY);
     fetch(url)
     .then((result)=>{
         return result.json();
@@ -21,10 +21,15 @@ let getConfig = function(){
      console.log('config:',data);
      console.log('success config get');   
     })
-}
+    .catch(function(err){
+        console.log(err);
+    });
+};
 function runSearch(searchMovie){
     let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', searchMovie);
-    fetch(url).then(result=>result.json()).then((data)=>{
+    fetch(url)
+    .then(result=>result.json())
+    .then((data)=>{
         document.getElementById('output').innerHTML = JSON.stringify(data, null, 4);
-    })
-}
+    });
+};
