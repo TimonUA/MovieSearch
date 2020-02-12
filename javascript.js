98321f0170b6a61b072d65d3ea1411b8f06dfe0a
TimonUA <57059072+TimonUA@users.noreply.github.com>
2020-02-12 20:18:23 +0200

    2

--- javascript.js
+++ javascript.js
@@ -6,30 +6,53 @@ $(document).ready(() => {
     });
 });
 let baseURL='https://api.themoviedb.org/3/';
-let configData = null;
-let baseImageURL = null;
+let configData = 'w300';
+let baseImageURL = 'http://image.tmdb.org/t/p/' ;
 let APIKEY='f7fae4e159c02dce183099b33d708df6';
-let getConfig = function(){
-    let url = "".concat(baseURL, 'configuration?api_key=',APIKEY);
-    fetch(url)
-    .then((result)=>{
-        return result.json();
-    })
-    .then((data)=>{
-     baseImageURL = data.images.secure_base_url;
-     configData = data.images;
-     console.log('config:',data);
-     console.log('success config get');   
-    })
-    .catch(function(err){
-        console.log(err);
-    });
-};
+// let getConfig = function(){
+//     let url = ''.concat(baseURL, 'configuration?api_key=',APIKEY);
+//     fetch(url)
+//     .then((result)=>{
+//         return result.json();
+//     })
+//     .then((data)=>{
+//      baseImageURL = data.images.secure_base_url;
+//      configData = data.images;
+//      console.log('config:',data);
+//      console.log('success config get');   
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     });
+// };
 function runSearch(searchMovie){
     let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', searchMovie);
     fetch(url)
     .then(result=>result.json())
     .then((data)=>{
-        document.getElementById('output').innerHTML = JSON.stringify(data, null, 4);
+        console.log(data);
+        let movies = data.results;
+        let output = '';
+        $.each(movies,(index,movie) => {
+            let imgURL
+            if(movie.poster_path){
+                imgURL = ''.concat(baseImageURL, configData, movie.poster_path);
+            }
+            else{
+                imgURL="images/movie_icon.png";
+            }           
+            output+= `
+            <div class="col-md-4">
+                <div class="well text-center justify-content-md-center">
+                      <img src="${imgURL}" style="size:300px"">
+                      <h5 class="lead align-middle text-center" style="color:white;  ">${movie.title}</h5>
+                </div>
+            </div>
+            `;
+        });
+        $('#movies').html(output);
+    })
+    .catch((err)=>{
+        console.log(err);
     });
 };
